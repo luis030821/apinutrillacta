3f92e0fd84df2f03d76528b8e65c2e3476a931d3
Luis <luisgarrido0987@gmail.com>
2025-01-25 21:21:08 -0500

    modify api

--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,6 @@ const apiKeyMiddleware = createMiddleware(async (c, next) => {
   return next();
 });
 
-// Route: Home
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
 // Route: Add user
 app.post("/user", apiKeyMiddleware, async (c) => {
   const { email } = await c.req.json();
@@ -44,7 +39,38 @@ app.post("/user", apiKeyMiddleware, async (c) => {
   await KV.put(email, JSON.stringify({ createdAt: new Date().toISOString() }));
   return c.json({ message: "User added successfully" }, 201);
 });
+app.get("/", apiKeyMiddleware, async (c) => {
+  const KV = c.env.nutrillacta;
+  const userData = await KV.list();
 
+  if (!userData) {
+    return c.json({ error: "User not found" }, 404);
+  }
+
+  return c.json(userData);
+});
+app.get("/images", async (c) => {
+  const KV = c.env.nutrillacta;
+  const userData = await KV.get("images");
+
+  if (!userData) {
+    return c.json({ error: "User not found" }, 404);
+  }
+  const jsonParse = JSON.parse(userData);
+  return c.json(jsonParse);
+});
+app.post("/images", apiKeyMiddleware, async (c) => {
+  const { img } = await c.req.json();
+  const KV = c.env.nutrillacta;
+  try {
+    await KV.put("images", JSON.stringify(img));
+    c.status(200);
+    return c.json({});
+  } catch (error) {
+    c.status(404);
+    return c.json({});
+  }
+});
 // Route: Get user
 app.get("/user", async (c) => {
   const email = c.req.query("email");
@@ -63,6 +89,20 @@ app.get("/user", async (c) => {
   }
 
   return c.json({ email, data: JSON.parse(userData) });
+});
+app.delete("/user", apiKeyMiddleware, async (c) => {
+  const { email } = await c.req.json();
+  if (email == c.env.EMAIL) {
+    return c.json({ email });
+  }
+  if (!email) {
+    return c.json({ error: "Email is required" }, 400);
+  }
+
+  const KV = c.env.nutrillacta;
+  await KV.delete(email);
+  c.status(200);
+  return c.json({ email });
 });
 
 export default app;
